f4706873f0fc515926cc00235bb8921d617c4c49
hikariyo <hikariyo1@outlook.com>
2026-01-10 22:24:34 +0800

    feat: basic repl

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
+#include "lexer.h"
+#include "parser.h"
+#include "term.h"
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, world\n");
+    char *input = NULL;
+
+    printf("Welcome to the REPL of lamcalc, a simple lambda calculus "
+           "interpreter.\n");
+    printf("Input \"exit\" without quotes to exit.\n");
+    while ((input = readline("> ")) != NULL) {
+        if (!strcmp(input, "exit")) {
+            free(input);
+            break;
+        }
+
+        if (*input != '\0') {
+            add_history(input);
+            token_t *tokens = lex_string(input);
+            token_t *cursor = tokens;
+
+            term_t *term = parse(&cursor, NULL);
+            if (term != NULL) {
+                term = term_eval(term);
+                char *repr = term_repr(term);
+                printf("%s\n", repr);
+                free(repr);
+                term_destroy(term);
+            }
+
+            lex_destroy_tokens(tokens);
+        }
+        free(input);
+    }
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -16,6 +16,11 @@ add_includedirs("include")
 target("lamcalc")
     set_kind("binary")
     add_files("src/*.c")
+    add_syslinks("readline")
+    if is_mode("debug") then
+        add_runenvs("ASAN_OPTIONS", "detect_leaks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_string_checks=1")
+        add_runenvs("UBSAN_OPTIONS", "print_stacktrace=1")
+    end
 
 target("tests")
     set_kind("binary")
