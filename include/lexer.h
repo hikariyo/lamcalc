1dd128e0eb156a0c9b551563c18ce7a8ea27afe3
hikariyo <hikariyo1@outlook.com>
2026-01-10 14:40:29 +0800

    docs: add detailed documentations of `lexer.h`

--- include/lexer.h
+++ include/lexer.h
@@ -4,22 +4,26 @@
 #include "symbol.h"
 
 typedef enum {
-    TOKEN_NAME,
-    TOKEN_LAMBDA,
-    TOKEN_DOT,
-    TOKEN_LP,
-    TOKEN_RP,
-    TOKEN_EOF,
+    TOKEN_NAME,   // Identifier (e.g., x, y, var_name)
+    TOKEN_LAMBDA, // Lambda symbol '\'
+    TOKEN_DOT,    // Function body separator '.'
+    TOKEN_LP,     // Left parenthesis '('
+    TOKEN_RP,     // Right parenthesis ')'
+    TOKEN_EOF,    // End of input/file marker
 } token_type_t;
 
 typedef struct token {
-    sym_t sym;
     token_type_t type;
-    struct token *next;
+    sym_t sym;          // Associated symbol (only valid for TOKEN_NAME)
+    struct token *next; // Pointer to the next token in the stream
 } token_t;
 
+// Lexes the input string and produces a linked-list of tokens.
+// Returns the head of the token stream.
+// Note: The caller takes ownership of the returned memory.
 token_t *lex_string(const char *str);
 
+// Recursively destroys the token stream and frees all associated memory.
 void lex_destroy_tokens(token_t *tokens);
 
 #endif // LEXER_H
