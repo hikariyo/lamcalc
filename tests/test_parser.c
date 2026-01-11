6266efe48be667556c9aeb9a91da272c8f0d7e66
hikariyo <hikariyo1@outlook.com>
2026-01-11 15:38:58 +0800

    test: simplify tests

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -23,32 +23,14 @@ TEST(parser_eof_position) {
 }
 
 TEST(parser_multiple_app) {
-    token_t *tokens = lex_string("f x y z");
-    token_t *cursor = tokens;
-
-    term_t *t = parse(&cursor, NULL);
+    term_t *t = parse_string("f x y z");
 
     TEST_ASSERT(t != NULL);
     TEST_ASSERT(t->type == TM_APP);
-    TEST_ASSERT(cursor == NULL);
+    TEST_ASSERT(t->data.app.right->type == TM_VAR);
+    TEST_ASSERT(t->data.app.right->data.var == sym_intern("z"));
 
     term_destroy(t);
-    lex_destroy_tokens(tokens);
-    return PASSED;
-}
-
-TEST(parser_multiple_app_parenthesis) {
-    token_t *tokens = lex_string("(((((((((f x) y) z)))))))");
-    token_t *cursor = tokens;
-
-    term_t *t = parse(&cursor, NULL);
-
-    TEST_ASSERT(t != NULL);
-    TEST_ASSERT(t->type == TM_APP);
-    TEST_ASSERT(cursor == NULL);
-
-    term_destroy(t);
-    lex_destroy_tokens(tokens);
     return PASSED;
 }
 
